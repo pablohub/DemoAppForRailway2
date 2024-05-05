321adbb93f4f64a8632cc3184c30f902528c8460
Pablo Castillo <pablocastillo.5.1992@gmail.com>
2024-05-05 18:03:15 -0500

    First commit

---
+++ src/main/java/com/pdev/demo/DemoAppForRailway2Application.java
@@ -0,0 +1,13 @@
+package com.pdev.DemoAppForRailway2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppForRailway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppForRailway2Application.class, args);
+	}
+
+}
